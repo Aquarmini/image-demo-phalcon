621647e5981188da4240b0c71ae2b586857b2511
李铭昕 <715557344@qq.com>
2018-01-27 17:57:30 +0800

    增加脚本目录

---
+++ app/tasks/Grafika/MainTask.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tasks\Grafika;
+
+use App\Tasks\Task;
+use Xin\Cli\Color;
+
+class MainTask extends Task
+{
+
+    public function mainAction()
+    {
+        echo Color::head('Help:') . PHP_EOL;
+        echo Color::colorize('  Grafika扩展测试') . PHP_EOL . PHP_EOL;
+
+        echo Color::head('Usage:') . PHP_EOL;
+        echo Color::colorize('  php run grafika:[command]', Color::FG_LIGHT_GREEN) . PHP_EOL . PHP_EOL;
+
+        echo Color::head('Commands:') . PHP_EOL;
+        echo Color::colorize('  main@test       测试', Color::FG_LIGHT_GREEN) . PHP_EOL;
+    }
+
+    public function testAction()
+    {
+        echo Color::colorize('Hello World', Color::FG_LIGHT_RED) . PHP_EOL;
+    }
+
+}
+
